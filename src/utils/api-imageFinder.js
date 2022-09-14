e3fda222a3eb03cb8308a8f3b07aeafe75224f45
Andrgoit <stakgrom@gmail.com>
2022-09-14 19:08:55 +0300

    hot fix

--- src/utils/api-imageFinder.js
+++ src/utils/api-imageFinder.js
@@ -1,4 +1,4 @@
-export function apiRequest(name, page) {
+export function apiRequest(query, page) {
   return fetch(
     `https://pixabay.com/api/?q=${query}&page=${page}&key=28166430-49d596e3415ce5cac11c6cb0f&image_type=photo&orientation=horizontal&per_page=12`
   )
